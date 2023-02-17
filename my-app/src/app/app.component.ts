7aee4f8bfb5ab7de4c81f34055a769d2eab75242
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-02-17 17:27:20 +0300

    Template variable with input field
    Added NgModule to app.module.ts

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import { ItemDetailsComponent } from './item-details/item-details.component';
-import { AppSizerComponent } from './app-sizer/app-sizer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 
 export class AppComponent 
 {
-  callPhone(phone:string)
-  {
-    console.log(phone);
-  }
+  firstExample:any;
 }
